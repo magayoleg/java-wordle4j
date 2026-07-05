3b28d28b89cdffa14a9e8b485cae6a38996e37a7
Magay Oleg <server90@mail.ru>
2026-07-05 18:15:50 +0300

    fix: Дополнил код тестами и добавил классы исключении

--- src/ru/yandex/practicum/InputException.java
+++ src/ru/yandex/practicum/InputException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class InputException extends Exception{
+public class InputException extends Exception {
     public InputException(final String message) {
         super(message);
     }
