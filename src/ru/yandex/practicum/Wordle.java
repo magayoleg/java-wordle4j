d1183a8372ea2ff7e12b29490e8d94a3bbca040f
Magay Oleg <server90@mail.ru>
2026-07-05 19:40:24 +0300

    fix: Дополнил код тестами и добавил классы исключении

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -16,8 +16,7 @@ import java.util.List;
  */
 public class Wordle {
     public static void main(String[] args) {
-        String projectDir = System.getProperty("user.dir");
-        try (PrintWriter printWriter = new PrintWriter(projectDir + "/log.txt", StandardCharsets.UTF_8)) {
+        try (PrintWriter printWriter = new PrintWriter("../../log.txt", StandardCharsets.UTF_8)) {
             WordleDictionaryLoader wordleDictionaryLoader = new WordleDictionaryLoader("\\words_ru.txt", printWriter);
             List<String> loaderDictionary = wordleDictionaryLoader.getDictionary();
 
